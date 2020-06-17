fc7683fcca091ba9d37b37a6440bb008b810e5d0
DohyunKIm <rlaehgus1229@gmail.com>
2020-06-17 22:47:15 +0900

    [SwiftUIPlayground] Add Custom Code

--- SwiftUIPlayground.playground/Contents.swift
+++ SwiftUIPlayground.playground/Contents.swift
@@ -3,7 +3,7 @@ import PlaygroundSupport
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, SwiftUI")
+        Text("Hello ").foregroundColor(.green).italic() + Text("SwiftUI").foregroundColor(.blue).bold()
     }
 }
 
