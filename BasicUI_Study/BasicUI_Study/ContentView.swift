ca463200716b9eec955c153122dc4018f40df4ea
DohyunKIm <rlaehgus1229@gmail.com>
2020-07-10 10:00:05 +0900

    [BasicUI_Study] Add VStack

--- BasicUI_Study/BasicUI_Study/ContentView.swift
+++ BasicUI_Study/BasicUI_Study/ContentView.swift
@@ -10,8 +10,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Text("도형 만들기").font(.largeTitle).fontWeight(.heavy)
+            Text("둥근 모양").font(.title)
+            Text("각진 모양").font(.title)
+        }.padding()
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
